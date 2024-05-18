3a2549adf3bce1f3307509a474ac93590a09fa12
Andres Giraldo <agiraldodev@gmail.com>
2024-05-17 21:09:21 -0500

    crear mostrar y eliminar un rol

--- src/controllers/Role.controller.ts
+++ src/controllers/Role.controller.ts
@@ -1,13 +1,61 @@
 import { Request, Response } from "express";
 import Role from "../models/Role.model";
 
+// Método para traer todos los roles de la tabla Roles
 export const mostrarRoles = async (req: Request, res: Response) => {
-  const listadoRoles = await Role.findAll()
-  res.json(listadoRoles);
+
+  try {
+    const listadoRoles = await Role.findAll(); // Traer todos los datos
+    res.json({ data: listadoRoles })
+  } catch (error) {
+    console.log(error)
+  }
+
 }
 
+// Método para insertar un nuevo role
 export const crearRole = async (req: Request, res: Response) => {
-  
-  const nuevoRole = new Role(req.body)
-  await nuevoRole.save()
+
+  try {
+    const nuevoRol = await Role.create(req.body);
+    res.json({ data: nuevoRol })
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
+// Método para mostrar el rol solo por su ID
+export const obtenerRolPorId = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params; // estudiantes/900
+
+    const role = await Role.findByPk(id)
+
+    if (!role) {
+      return res.status(404).json({
+        error: 'El rol no ha sido encontrado'
+      })
+    }
+
+    res.json({ data: role })
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Método para eliminar el rol por su ID
+export const borrarRol = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const role = await Role.findByPk(id)
+
+  if (!role) {
+    return res.status(404).json({
+      error: 'El rol no ha sido encontrado'
+    })
+  }
+
+  await role.destroy()
+  res.json({ data: 'El rol fue eliminado satisfactoriamente' })
 }
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { mostrarRoles, crearRole } from "./controllers/Role.controller";
+import { mostrarRoles, crearRole, obtenerRolPorId, borrarRol } from "./controllers/Role.controller";
 
 const router = Router();
 
 router.get('/roles', mostrarRoles)
 router.post('/roles', crearRole )
+router.get('/roles/:id', obtenerRolPorId )
+router.delete('/roles/:id', borrarRol)
 
 export default router;
